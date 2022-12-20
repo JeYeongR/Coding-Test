889e42ed2c3de8e731c46d96c457f735759893ea
JeYeongR <113500815+JeYeongR@users.noreply.github.com>
2022-12-20 17:12:40 +0900

    [level 2] Title: 주식가격, Time: 11.39 ms, Memory: 62.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.java
@@ -0,0 +1,18 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+
+        for (int i = 0; i < prices.length; i++) {
+            for (int j = 1 + i; j < prices.length; j++) {
+                answer[i]++;
+                if (prices[i] > prices[j]) {
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+}
