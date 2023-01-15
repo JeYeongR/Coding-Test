7202ed499b09e3ccabbdde89c3acbf81dd90cf22
JeYeongR <113500815+JeYeongR@users.noreply.github.com>
2023-01-16 03:45:20 +0900

    [level 3] Title: 정수 삼각형, Time: 11.00 ms, Memory: 61.4 MB -BaekjoonHub

--- 프로그래머스/lv3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/lv3/43105. 정수 삼각형/정수 삼각형.java
@@ -1,4 +1,5 @@
 class Solution {
+
     // 1. 삼각형의 꼭대기인 triangle[0][0]은 공통으로 들어감
     // 2. 두번째 줄부터 자신과 위에 있는 숫자를 더 함
     //   2-1. 왼쪽 끝 숫자면 위에 있는 숫자는 오른쪽 대각선만 있음
@@ -6,21 +7,23 @@ class Solution {
     //   2-3. 중앙에 있는 숫자면 위에 있는 숫자는 오른쪽, 왼쪽 대각선에 둘 다 있으니 둘 중 더해서 큰 값으로 정함
     public static int solution(int[][] triangle) {
         int answer = 0;
-        // 1. 삼각형의 꼭대기인 triangle[0][0]은 공통으로 들어감 
+        int[][] dp = new int[triangle.length][triangle.length];
+        // 1. 삼각형의 꼭대기인 triangle[0][0]은 공통으로 들어감
+        dp[0][0] = triangle[0][0];
         for (int i = 1; i < triangle.length; i++) { // 2. 두번째 줄부터 자신과 위에 있는 숫자를 더 함
             for (int j = 0; j < triangle[i].length; j++) {
                 if (j == 0) { // 2-1. 왼쪽 끝 숫자면 위에 있는 숫자는 오른쪽 대각선만 있음
-                    triangle[i][j] += triangle[i - 1][j];
+                    dp[i][j] = triangle[i][j] + dp[i - 1][j];
                 } else if (j == i) { // 2-2. 오른쪽 끝 숫자면 위에 있는 숫자는 왼쪽 대각선만 있음
-                    triangle[i][j] += triangle[i - 1][j - 1];
-                } else { // 2-3. 중앙에 있는 숫자면 위에 있는 숫자는 오른쪽, 왼쪽 대각선에 둘 다 있으니 둘 중 더해서 큰 값으로 정함 
-                    triangle[i][j] += Math.max(triangle[i - 1][j], triangle[i-1][j-1]);
+                    dp[i][j] = triangle[i][j] +  dp[i - 1][j - 1];
+                } else { // 2-3. 중앙에 있는 숫자면 위에 있는 숫자는 오른쪽, 왼쪽 대각선에 둘 다 있으니 둘 중 더해서 큰 값으로 정함
+                    dp[i][j] = triangle[i][j] +  Math.max(dp[i - 1][j], dp[i-1][j-1]);
                 }
 
-                answer = Math.max(answer, triangle[i][j]);
+                answer = Math.max(answer, dp[i][j]);
             }
         }
 
-        return answer;
+    return answer;
     }
 }
