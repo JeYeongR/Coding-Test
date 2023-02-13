ac5afd390f2a64216a80d95c383ab7c9dd968fa1
JeYeongR <113500815+JeYeongR@users.noreply.github.com>
2023-02-13 20:48:04 +0900

    [Silver III] Title: 01타일, Time: 136 ms, Memory: 18092 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] dp = new int[1000001];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 3;
+
+        for (int i = 4; i <= N; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 15746;
+        }
+        System.out.println(dp[N]);
+    }
+}
