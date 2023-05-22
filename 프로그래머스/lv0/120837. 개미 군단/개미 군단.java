c8889a750c5112ecc3fefde51dc16414736a9af0
JeYeongR <113500815+JeYeongR@users.noreply.github.com>
2023-05-22 14:58:22 +0900

    [level 0] Title: 개미 군단, Time: 0.02 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120837. 개미 군단/개미 군단.java
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.java
@@ -2,16 +2,14 @@ class Solution {
     public int solution(int hp) {
         int answer = 0;
         
-        while(hp >= 5) {
-            hp -= 5;
+        while(hp > 0) {
+            if(hp >= 5) hp -= 5;
+            else if(hp >= 3) hp -= 3;
+            else hp -= 1;
+            
             answer++;
         }
         
-        while(hp >= 3) {
-            hp -= 3;
-            answer++;
-        }
-        
-        return answer + hp;
+        return answer;
     }
 }
