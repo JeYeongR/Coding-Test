8b8e0eafbca12323e83d995cdd2819740fa4fa6d
JeYeongR <113500815+JeYeongR@users.noreply.github.com>
2023-06-03 13:21:32 +0900

    [unrated] Title: rny_string, Time: 0.02 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181863. rny＿string/rny＿string.java
+++ 프로그래머스/unrated/181863. rny＿string/rny＿string.java
@@ -1,16 +1,5 @@
 class Solution {
     public String solution(String rny_string) {
-        String answer = "";
-        
-        for(char c : rny_string.toCharArray()) {
-            if(c == 'm') {
-                answer += 'r';
-                answer += 'n';
-            } else {
-                answer += c;
-            }
-        }
-        
-        return answer;
+        return rny_string.replace("m", "rn");
     }
 }
