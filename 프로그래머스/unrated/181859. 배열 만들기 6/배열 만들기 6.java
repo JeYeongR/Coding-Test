74ece199bec7e9de52b3ccaa1f7703369c185211
JeYeongR <113500815+JeYeongR@users.noreply.github.com>
2023-06-04 15:38:24 +0900

    [unrated] Title: 배열 만들기 6, Time: 18.55 ms, Memory: 86.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181859. 배열 만들기 6/배열 만들기 6.java
+++ 프로그래머스/unrated/181859. 배열 만들기 6/배열 만들기 6.java
@@ -3,24 +3,20 @@ import java.util.Stack;
 class Solution {
     public int[] solution(int[] arr) {
         Stack<Integer> stk = new Stack<>();
-        
-        for(int i=0; i<arr.length; i++) {
-            if(stk.isEmpty()) {
-                stk.push(arr[i]);
+
+        for (int j : arr) {
+            if (stk.isEmpty() || stk.peek() != j) {
+                stk.push(j);
             } else {
-                if(stk.peek() == arr[i]) {
-                    stk.pop();
-                } else {
-                    stk.push(arr[i]);
-                }   
+                stk.pop();
             }
         }
-        
+
         int[] answer = new int[stk.size()];
         for (int i = stk.size() - 1; i >= 0; i--) {
             answer[i] = stk.pop();
         }
 
-        return answer.length !=0 ? answer : new int[]{-1};
+        return answer.length != 0 ? answer : new int[]{-1};
     }
 }
