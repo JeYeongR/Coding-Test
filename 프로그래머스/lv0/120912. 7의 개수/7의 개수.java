9d7cc0c3739ff2772e466596f29822d01879c7e0
JeYeongR <113500815+JeYeongR@users.noreply.github.com>
2023-06-04 16:06:47 +0900

    [level 0] Title: 7의 개수, Time: 0.03 ms, Memory: 66.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120912. 7의 개수/7의 개수.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        
+        for(int num : array) {
+            while(num>0) {
+                if(num%10==7) answer++;
+                
+                num/=10;
+            }
+        }
+        
+        return answer;
+    }
+}
