bb8205ba0b0b5e8a82a6f7490005b9ca629a012e
JeYeongR <113500815+JeYeongR@users.noreply.github.com>
2023-06-07 13:58:32 +0900

    [level 0] Title: 평행, Time: 0.01 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120875. 평행/평행.java
+++ 프로그래머스/lv0/120875. 평행/평행.java
@@ -1,19 +1,23 @@
 class Solution {
     public int solution(int[][] dots) {
         int answer = 0;
-        
-        if ((double) (dots[0][1] - dots[1][1]) / (dots[0][0] - dots[1][0]) == (double) (dots[2][1] - dots[3][1]) / (dots[2][0] - dots[3][0])) {
+
+        if (slopeCalculation(dots[0], dots[1]) == slopeCalculation(dots[2], dots[3])) {
             answer = 1;
         }
 
-        if ((double) (dots[0][1] - dots[2][1]) / (dots[0][0] - dots[2][0]) == (double) (dots[1][1] - dots[3][1]) / (dots[1][0] - dots[3][0])) {
+       if (slopeCalculation(dots[0], dots[2]) == slopeCalculation(dots[1], dots[3])) {
             answer = 1;
         }
 
-        if ((double) (dots[0][1] - dots[3][1]) / (dots[0][0] - dots[3][0]) == (double) (dots[1][1] - dots[2][1]) / (dots[1][0] - dots[2][0])) {
+       if (slopeCalculation(dots[0], dots[3]) == slopeCalculation(dots[1], dots[2])) {
             answer = 1;
         }
 
         return answer;
+    }
+
+    public double slopeCalculation(int[] dots1, int[] dots2) {
+        return (double) (dots1[1] - dots2[1]) / (dots1[0] - dots2[0]);
     }
 }
