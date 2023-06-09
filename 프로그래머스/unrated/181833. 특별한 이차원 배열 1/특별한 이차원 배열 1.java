4993447e3812f5756412ae91600e2fecda57e188
JeYeongR <113500815+JeYeongR@users.noreply.github.com>
2023-06-09 12:33:06 +0900

    [unrated] Title: 특별한 이차원 배열 1, Time: 0.09 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.java
+++ 프로그래머스/unrated/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.java
@@ -2,17 +2,10 @@ class Solution {
     public int[][] solution(int n) {
         int[][] answer = new int[n][n];
         
-        int idx = 0;
         for(int i=0; i<n; i++) {
-            for(int j=0; j<n; j++) {
-                if(j==idx) {
-                    answer[i][j] = 1;
-                } else {
-                    answer[i][j] = 0;
-                }
-            }
-            idx++;
+           answer[i][i] = 1;  
         }
+        
         return answer;
     }
 }
