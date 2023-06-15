b207b73cb22b500af8bc4a8ec7232e516680eed2
JeYeongR <113500815+JeYeongR@users.noreply.github.com>
2023-06-15 21:22:20 +0900

    [unrated] Title: 덧칠하기, Time: 0.84 ms, Memory: 83.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int n, int m, int[] section) {
+        int answer = 0;
+        int max = 0;
+        
+        for(int i=0; i<section.length; i++) {
+            if(section[i] > max) {
+                max = section[i] + m - 1;
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
