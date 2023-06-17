b2347ead82435c3ebe975e8ad289cc5c29ddfbb8
JeYeongR <113500815+JeYeongR@users.noreply.github.com>
2023-06-18 03:11:40 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 3.00 ms, Memory: 82.2 MB -BaekjoonHub

--- 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -5,24 +5,17 @@ class Solution {
     public int[] solution(String today, String[] terms, String[] privacies) {
         List<Integer> list = new ArrayList<>();
         String[] day = today.split("\\.");
-        LocalDate localdate = LocalDate.of(Integer.parseInt(day[0]), Integer.parseInt(day[1]), Integer.parseInt(day[2]));
+        int todayCount = Integer.parseInt(day[0]) * 12 * 28 + Integer.parseInt(day[1]) * 28 + Integer.parseInt(day[2]);
 
         for (String term : terms) {
             String[] split = term.split(" ");
             for (int j = 0; j < privacies.length; j++) {
                 if (privacies[j].contains(split[0])) {
                     String[] privacy = privacies[j].split(" ")[0].split("\\.");
-                    int countYear = Integer.parseInt(privacy[0]);
                     int plusMonth = Integer.parseInt(privacy[1]) + Integer.parseInt(split[1]);
-                    if (plusMonth > 12) {
-                        countYear += plusMonth / 12;
-                        plusMonth %= 12;
-                    }
+                    int privacyDayCount = Integer.parseInt(privacy[0]) * 12 * 28 + plusMonth * 28 + Integer.parseInt(privacy[2]) - 1;
 
-                    plusMonth = plusMonth == 0 ? 1 : plusMonth;
-                    int countDay = Integer.parseInt(privacy[2]) - 1 <= 0 ? 28 : Integer.parseInt(privacy[2]) - 1;
-                    LocalDate privacyDay = LocalDate.of(countYear, plusMonth, countDay);
-                    if (privacyDay.isBefore(localdate)) list.add(j + 1);
+                    if (privacyDayCount < todayCount) list.add(j + 1);
                 }
             }
         }
