afe43df5c78ac5cbedb1994d31a9789c581a3bf8
JeYeongR <113500815+JeYeongR@users.noreply.github.com>
2023-06-21 20:12:11 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.12 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public long solution(int n) {
+        long[] dp = new long[n+2];
+        dp[1] = 1;
+        dp[2] = 2;
+        
+        for(int i=3; i<=n; i++) {
+            dp[i] = (dp[i-1] + dp[i-2]) % 1234567;
+        }
+        
+        return dp[n];
+    }
+}
