99c886189c1ff7d3523fa10b23703700638f2398
JeYeongR <113500815+JeYeongR@users.noreply.github.com>
2023-07-02 18:45:41 +0900

    [Bronze V] Title: 아스키 코드, Time: 204 ms, Memory: 17636 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String c = sc.next();
+
+        int answer = c.charAt(0);
+
+        System.out.println(answer);
+    }
+}
