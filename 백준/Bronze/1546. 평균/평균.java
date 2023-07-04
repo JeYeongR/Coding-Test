9078aafa067380c752c9c405b4b4fa3951fd375b
JeYeongR <113500815+JeYeongR@users.noreply.github.com>
2023-07-04 21:55:05 +0900

    [Bronze I] Title: 평균, Time: 260 ms, Memory: 18860 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        int[] arr = new int[N];
+        int max = 0;
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+            max = Math.max(max, arr[i]);
+        }
+
+        float sum = 0;
+        for (int i = 0; i < N; i++) {
+            sum += (float) arr[i] / max * 100;
+        }
+
+        System.out.println(sum / N);
+    }
+}
