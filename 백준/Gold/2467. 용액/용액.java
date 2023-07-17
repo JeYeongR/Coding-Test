1f82ecb22bbf278081fddc09bc71e5459645e2d6
JeYeongR <113500815+JeYeongR@users.noreply.github.com>
2023-07-17 17:32:36 +0900

    [Gold V] Title: 용액, Time: 380 ms, Memory: 28476 KB -BaekjoonHub

---
+++ 백준/Gold/2467. 용액/용액.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] answer = new int[2];
+        int min = Integer.MAX_VALUE;
+        int lt = 0;
+        int rt = N - 1;
+        while (lt < rt) {
+            int sum = Math.abs(arr[lt] + arr[rt]);
+            if (sum < min) {
+                min = sum;
+                answer[0] = arr[lt];
+                answer[1] = arr[rt];
+            }
+
+            if (arr[lt] + arr[rt] > 0) rt--;
+            else lt++;
+        }
+        for (int num : answer) {
+            System.out.print(num + " ");
+        }
+    }
+}
