c964752648b83df2c6fe969addf1029fc5df7c4c
JeYeongR <113500815+JeYeongR@users.noreply.github.com>
2023-07-26 17:09:21 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 2172 ms, Memory: 186612 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        ArrayList<Point> arrayList = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            int x = in.nextInt();
+            int y = in.nextInt();
+            arrayList.add((new Point(x, y)));
+        }
+
+        Collections.sort(arrayList);
+
+        for (Point point : arrayList) {
+            System.out.println(point.x + " " + point.y);
+        }
+    }
+}
+
+class Point implements Comparable<Point> {
+
+    int x;
+    int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public int compareTo(Point o) {
+        if (this.x == o.x) return this.y - o.y;
+        else return this.x - o.x;
+    }
+}
