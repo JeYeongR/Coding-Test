7721294224b7cda63f507bef3e7e4a9a4a56ecfe
JeYeongR <113500815+JeYeongR@users.noreply.github.com>
2023-08-07 15:34:13 +0900

    [Silver V] Title: 영화감독 숌, Time: 384 ms, Memory: 88892 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        int num = 666;
+        int count = 1;
+
+        while (count != N) {
+            num++;
+            
+            if (String.valueOf(num).contains("666")) count++;
+        }
+
+        System.out.println(num);
+    }
+}
