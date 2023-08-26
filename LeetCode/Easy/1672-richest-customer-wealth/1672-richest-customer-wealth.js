00427277e33428adcae60e62a56339722039ead0
JeYeongR <113500815+JeYeongR@users.noreply.github.com>
2023-08-26 14:37:40 +0900

    Time: 54 ms (64.66%), Space: 41.3 MB (99.08%) - LeetHub

---
+++ LeetCode/Easy/1672-richest-customer-wealth/1672-richest-customer-wealth.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[][]} accounts
+ * @return {number}
+ */
+var maximumWealth = function(accounts) {
+    let max = 0;
+    for(let i=0; i<accounts.length; i++) {
+        let sum = 0;
+        for(let j=0; j<accounts[i].length; j++) {
+            sum += accounts[i][j];
+        }
+        if(sum > max) {
+            max = sum;
+        }
+    }
+    
+    return max;
+};
