37882a6abe39158bf05f21ca4520955066516957
JeYeongR <113500815+JeYeongR@users.noreply.github.com>
2023-08-28 13:58:20 +0900

    Time: 50 ms (78.52%), Space: 41.5 MB (89.33%) - LeetHub

---
+++ LeetCode/Easy/1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var numberOfSteps = function(num) {
+    let count = 0;
+    while(num > 0) {
+        num = num % 2 === 0 ? num / 2 : num - 1;
+        count++;
+    }
+    
+    return count;
+};
