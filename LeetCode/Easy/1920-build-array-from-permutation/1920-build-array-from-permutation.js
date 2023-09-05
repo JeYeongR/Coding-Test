af485eaff094392615e0f29a533c527559f5bc6f
JeYeongR <113500815+JeYeongR@users.noreply.github.com>
2023-09-05 09:56:07 +0900

    Time: 63 ms (87.28%), Space: 46.4 MB (27.86%) - LeetHub

---
+++ LeetCode/Easy/1920-build-array-from-permutation/1920-build-array-from-permutation.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var buildArray = function(nums) {
+    const ans = [];
+    for(let i = 0; i<nums.length; i++) {
+        ans[i] = nums[nums[i]];
+    }
+    
+    return ans;
+};
