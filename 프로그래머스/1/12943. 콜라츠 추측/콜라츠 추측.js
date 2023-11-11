ada849462beaeddb57cb66152dd5bc73978f51d2
JeYeongR <113500815+JeYeongR@users.noreply.github.com>
2023-11-11 14:43:36 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -3,12 +3,8 @@ function solution(num) {
     
     let count = 0;
     while(num !== 1 && count !== 500) {
-        if(num % 2 == 0) {
-            num /= 2;
-        } else {
-            num *= 3;
-            num++;
-        }
+        num % 2 == 0 ? num /= 2 : num = num * 3 + 1; 
+
         count++;
     }
     
