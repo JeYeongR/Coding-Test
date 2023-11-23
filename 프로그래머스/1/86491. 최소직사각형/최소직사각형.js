368c787643afd1390a5a0c3b9b7c06cd460d62b5
JeYeongR <113500815+JeYeongR@users.noreply.github.com>
2023-11-23 15:29:20 +0900

    [level 1] Title: 최소직사각형, Time: 6.27 ms, Memory: 41.7 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -1,12 +1,10 @@
 function solution(sizes) {
-    let answer = 0;
     let width = 0;
     let length = 0;
-    for(let i = 0; i < sizes.length; i++) {
-        let tempWidth = Math.max(sizes[i][0], sizes[i][1]);
-        let tempLength = Math.min(sizes[i][0], sizes[i][1]);
-        width = Math.max(width, tempWidth);
-        length = Math.max(length, tempLength);
-    }
+    sizes.forEach(s => {
+        const [a, b] = s.sort((a, b) => a - b);
+        width = Math.max(a, width);
+        length = Math.max(b, length);
+    });
     return width * length;
 }
