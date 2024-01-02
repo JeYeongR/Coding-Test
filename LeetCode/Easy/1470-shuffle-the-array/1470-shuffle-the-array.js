467f714c40aacb4e8a1e935d419fec0dd46d89a1
JeYeongR <113500815+JeYeongR@users.noreply.github.com>
2024-01-02 14:30:46 +0900

    Time: 121 ms (5.12%), Space: 44.4 MB (43.79%) - LeetHub

---
+++ LeetCode/Easy/1470-shuffle-the-array/1470-shuffle-the-array.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @param {number} n
+ * @return {number[]}
+ */
+var shuffle = function(nums, n) {
+    let result = [];
+    for(let i = 0; i < n; i++) {
+        result.push(nums[i], nums[i + n]);
+    }
+    return result;
+};
