5cd58479b1bfef1fa308f50fa9339912bedd1d47
JeYeong <113500815+JeYeongR@users.noreply.github.com>
2024-03-11 20:38:44 +0900

    [level 1] Title: 모의고사, Time: 5.24 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=python3) 
 
 ### 성능 요약
 
-메모리: 37.2 MB, 시간: 5.53 ms
+메모리: 10.3 MB, 시간: 5.24 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 5일 16:6:43
+2024년 03월 11일 20:38:40
 
 ### 문제 설명
 
---
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -0,0 +1,21 @@
+def solution(answers):
+    patterns = [
+        [1, 2, 3, 4, 5],
+        [2, 1, 2, 3, 2, 4, 2, 5],
+        [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    ]
+    scores = [0] * 3
+    
+    for i, answer in enumerate(answers):
+        for j, pattern in enumerate(patterns):
+            if answer == pattern[i % len(pattern)]:
+                scores[j] += 1
+    
+    max_score = max(scores)
+    highest_scores = []
+    
+    for i, score in enumerate(scores):
+        if score == max_score:
+            highest_scores.append(i + 1)
+            
+    return highest_scores
